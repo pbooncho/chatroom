f54269b7ff769778f23cfdf2dc2b831da45b27df
eliath <eliasmartinezcohen@gmail.com>
2014-04-18 19:26:46 -0400

    Switched from AJAX to using socket.io

--- pub/js/room.js
+++ pub/js/room.js
@@ -1,33 +1,63 @@
+var socket = io.connect();
+
 var prev_msg_ID = 0,
 	message_load_interval,
-	new_message = false,
-	open_tag	=	"<p class='singleMsg'>", //for crafting single messages
-	open_span	=		"<span class='singleMsgNick'>", //holds the username
-	close_span	=		"</span>",
-	close_tag	=	"</p>";
+	new_message = false;
 
 /**
  * attach a new handler for the submit
  * function of the room form.
  */
 $(window).load(function() {
+	/* handle incoming socket events from server: */
+	//handle new message:
+	socket.on('message', function(nickname, message) {
+		$("#messageList").append(HTMLifyMessage(nickname, message)); //HTMLify & append message
+		scrollList(); //scroll new message into view
+	});
+
+	//handle when room's members change:
+	socket.on('membershipChanged', function(userlist) {
+		var userlistHTML = "<h2>Online Now</h2>\n";
+		for (var i = 0; i < userlist.length; i++) {
+			userlistHTML += HTMLifyUser(userlist[i].active, userlist[i].nickname);
+		}
+		console.log("USERLIST HTML:\n", userlistHTML);
+		$("#userlist").html(userlistHTML);
+	});
+
+	socket.on('disconnect', function () {
+		alert('The server has disconnected.');
+		document.location.reload(true);
+	});
+
+
+	/* now bind form submits: */
     $("#messageForm").submit(sendMessage);
     $("#nick_form").submit(setNick);
-
-/*
-    //prevent scrolling to into the spacer
-    msgL = $("#messageList");
-	msgL.scroll(function(event) {
-		var st = msgL.scrollTop(),
-			h  = msgL.height();
-    	if (st < h) msgL.scrollTop(h);
-    });
-*/
-    //set the timeout to start loading messages:
-    startLoadingMsgs();
-    //loadAllMsgs();
-
 });
+
+
+/**
+ * Tells the server that this client
+ * is no longer focused in the window,
+ * so that the server can update the userlist.
+ */
+window.onblur = function() {
+	socket.emit('blur');
+};
+
+
+/**
+ * Tells the server that this client
+ * is now focused in the window, so that
+ * they can be marked as available in the
+ * user list.
+ */
+window.onfocus = function() {
+	socket.emit('focus');
+};
+
 
 /**
  * A function to send a new message from the
@@ -37,28 +67,11 @@ $(window).load(function() {
  */
 function sendMessage(e) {
 	e.preventDefault(); //no redirect
-	//get pertinent data from form:
-	var msgInput = $("#messageField"),
-		msgData = {};
 
-	var unsafe = msgInput.val();
-	msgData.message = toMsgString(unsafe);
-	msgInput.val(""); //immediately remove text from input
-	msgData.nickname = $("#nickField").val();
+	var message = $("#messageField").val();		//get message body
+	socket.emit('message', message);			//send message via socket
 
-	var roomName = $("meta[name=roomName]").attr('content'); //get room name
-	console.log("Sending message:", msgData.message);
-	
-	//AJAX
-	$.post('/'+ roomName +'/messages', msgData, function(resp) {
-		if (resp.success) { //'success' here refers to the database successfully inserting the message into the table
-			var new_message_open_tag = "<p id='new_message' class='singleMsg'>"; //give open tag an ID; need to remove it later
-			var msg_html = new_message_open_tag + open_span + msgData.nickname + ":" + close_span + msgData.message + close_tag; //craft HTML
-			$("#messageList").append(msg_html); //append message
-			new_message = true; //flag for removal on next request to server
-		}
-	});
-	scrollList();
+	$("#messageField").val(""); //remove text from input
 }
 
 
@@ -75,96 +88,25 @@ function setNick(e) {
 		$("#nick_prompt").append("<p>Username must be between 3 and 16 characters.</p>");
 		return;
 	}
+
+	$("#nickLabel").text(nick+":");					//set nickname label
+	console.log("nickname set to:", nick);			//log nickname to console
 	
+	//clean up the view (remove nickname prompt)
 	$("#nick_prompt p").remove(); //the nick is ok, remove the err msg
-	$("#nickField").val(nick); //set nickname field in form
-	$("#nickLabel").text(nick+":"); //set nickname label
-	var pdW = $("#nickLabel").width(); //set nickname label
-	//$("#messageList").css('padding-left', $("#nickLabel").width()); // set padding left to width of nickname
-	console.log("nickname set to:", $("#nickField").val());
 	$("#nick_overlay").fadeOut('400', function() {
 		$("#nick_overlay").remove();
 	});
-}
 
-
-
-
-
-function loadSincePrev() {
-	var data = {prevID:prev_msg_ID}; //set data.prevID to the most recent message ID.
-	var roomName = $("meta[name=roomName]").attr('content');//get room name
-
-	//AJAX: send the most recently displayed message ID to the server 
-	//so it can select only the undisplayed messages from the db.
-	$.post('/'+ roomName +'/messages/prev', data, function(resp) {
-		if (resp.success) {} //TODO: perhaps try to handle errors ?		
-		// new message gets set to true when we add
-		// a new message to the message list.
-		// new message comes from the UI, not the database
-		// so we need to remove it before appending 
-		// messages from the database to the message list, 
-		// otherwise we will get duplicates.
-		if (new_message) {
-			$("#new_message").remove();
-			new_message = false;
-		}
-
+	//now join the room via the socket (implicitly sets the nickname) 
+	socket.emit('join', meta('roomName'), nick, function(messages) {
 		//loop through message array and append each message to messageList
-		var msg;
-		for (var i = 0; i < resp.msgArray.length; i++) {
-			msg = resp.msgArray[i];
-			var msg_html = open_tag + open_span + msg.nickname + ":" + close_span + msg.body + close_tag; //craft HTML
-			$("#messageList").append(msg_html); //append message
+		for (var i = 0; i < messages.length; i++) {
+			//HTMLify & append messageq
+			$("#messageList").append(HTMLifyMessage(messages[i].nickname, messages[i].body));
 		}
-
-		//store id of most recently displayed message (if there is one)
-		if (msg) {
-			prev_msg_ID = msg.id;
-			scrollList(); //scroll only when there's a new message
-		}
+		scrollList();
 	});
-}
-
-function loadAllMsgs() {
-	var roomName = $("meta[name=roomName]").attr('content');//get room name
-
-	//AJAX: send the most recently displayed message ID to the server 
-	//so it can select only the undisplayed messages from the db.
-	$.get('/'+ roomName +'/messages', function(resp) {
-		if (resp.success) {} //TODO: perhaps try to handle errors ?		
-		// new message gets set to true when we add
-		// a new message to the message list.
-		// new message comes from the UI, not the database
-		// so we need to remove it before appending 
-		// messages from the database to the message list, 
-		// otherwise we will get duplicates.
-		if (new_message) {
-			$("#new_message").remove();
-			new_message = false;
-		}
-
-		var msg;
-		//loop through message array and append each message to messageList
-		for (var i = 0; i < resp.msgArray.length; i++) {
-			msg = resp.msgArray[i];
-			var msg_html = open_tag + open_span + msg.nickname + ":" + close_span + msg.body + close_tag; //craft HTML
-			$("#messageList").append(msg_html); //append message
-		}
-		if (msg)
-			prev_msg_ID = msg.id;
-	});
-	scrollList();
-}
-
-function startLoadingMsgs() {
-	message_load_interval = window.setInterval(loadSincePrev, 500);
-}
-/**
- * pauses the message loader
- */
-function pause() {
-	clearInterval(message_load_interval);
 }
 
 
@@ -173,31 +115,50 @@ function scrollList() {
 	$("#messageList").scrollTop(scrollAmount);
 }
 
-function toMsgString(unsafe) {
-	var url_regex = /^(http|https)\:\/\/[\w\d\-_]+(\.[\w\d\-_]+)+([\w\-\.,@?^=%&amp;\:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/; //url matching regex i found 
-	var url_arr = unsafe.match(url_regex); //test if its a url
-	
-	if (url_arr) {
-		if (url_arr[url_arr.length-1].match(/(.png|.gif|.jpeg|.jpg)/)) {
-			//it's an image!
-			return "<img src='" + unsafe + "'>";
-		} else {
-			//its just a url
-			return "<a href='" + unsafe + "'>" + unsafe + "</a>";
-		}
-	} 
-
-	else {
-		//it's not a URL. convert it to an HTML-escaped string
-		return escapeHtml(unsafe);
-	}
+/**
+ * Create an HTML entity for a new message to be displayed
+ * 
+ * @param  {String} nickname - Nickname of the message sender
+ * @param  {String} message  - The content of the message
+ * @return {String} An HTML string of the nickname & message
+ */
+function HTMLifyMessage(nickname, message) {
+	var msg_html = "<p class='singleMsg'>" + 
+						"<span class='singleMsgNick'>" + 
+							nickname + ":" + 
+						"</span>" +
+						message + 
+					"</p>";
+	return msg_html;
 }
 
-function escapeHtml(unsafe) {
-    return unsafe
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
- }
+/**
+ * Creates an HTML entity for a user
+ * @param {Boolean} available - if the user is available or not
+ * @param {String} nickname - the nickname of the user
+ * @return {String} An HTML string of the avail. identifier and username
+ */
+function HTMLifyUser(available, nickname) {
+	var avail_tag;
+	if (available)
+		avail_tag = "<div class='status available'>";
+	else
+		avail_tag = "<div class='status idle'>";
+
+	var user_html = "<div class='singleUser'>\n" +
+						avail_tag + "</div>\n" +
+						"<h1>" +
+							nickname +
+						"</h1>\n" +
+					"</div>\n";
+	return user_html;
+}
+
+/**
+ * Cool helper function to let you grab info from the meta tag.
+ * @param  {String} tagName - The name of the meta tag to get the content of
+ * @return {String}				The content of the meta tag with the specified name
+ */
+function meta(tagName) {
+	return $("meta[name="+ tagName +"]").attr('content');
+}
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*jslint smarttabs:true */
 /**
  * @author eliath <http://eliasmartinezcohen.com/>
  * @package chatroom
@@ -9,11 +10,19 @@
 /* 
    Server setup
    ========================================================================== */
+
 //set up express
 var express = require('express'),
 	app = express(),
 	colors = require('colors'),
 	engines = require('consolidate');
+//set up socket.io stuff
+var http = require('http'),
+	server = http.createServer(app);
+
+//now add socket.io
+var io = require('socket.io').listen(server);
+io.set('log level', 1); //debug mode off
 
 //json, urlencoded, multipart middleware:
 app.use(express.bodyParser());
@@ -25,11 +34,26 @@ app.set('views', __dirname + "/templates"); //templating
 
 
 /* 
-   Database setup
+   Packaged Queries
+   ========================================================================== */
+/* DB Creation */
+var createRoomsTable = 'CREATE TABLE IF NOT EXISTS rooms (name TEXT PRIMARY KEY);';
+var createMessagesTable = 'CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, room TEXT, nickname TEXT, body TEXT, time INTEGER);';
+
+/* Find shit */
+var getRoomByName = 'SELECT name FROM rooms WHERE name=$1;';
+var getRecentRooms = "SELECT DISTINCT room FROM messages WHERE time >= strftime('%s', 'now') - 300;"; //300 seconds = 5 min
+var getMessagesInRoom = 'SELECT * FROM messages WHERE room=$1;';
+
+/* Insert shit */
+var insertRoom = 'INSERT INTO rooms VALUES ($1);';
+var insertMessage = 'INSERT INTO messages(room, nickname, body, time) VALUES($1, $2, $3, $4);';
+
+/* 
+   More Database setup
    ========================================================================== */
 var anyDB = require('any-db');
 var conn = anyDB.createConnection('sqlite3://chatroom.db');
-
 
 
 /* 
@@ -39,14 +63,15 @@ var conn = anyDB.createConnection('sqlite3://chatroom.db');
 app.get('/', function(req, resp) {
 	console.log('- Request received:', req.method.cyan, req.url.underline);
 	
-	//TODO: maybe someday we can have users sign in here and give them a cookie
+	//TODO: maybe someday we can have users sign in here 
+	//      and give them a session cookie
 
 	//create new Table for messages and rooms:
-	conn.query('CREATE TABLE IF NOT EXISTS rooms (name TEXT PRIMARY KEY);')
+	conn.query(createRoomsTable)
 		.on('error', function() {
 			console.log("ERROR: rooms table could not be created!!".error);
 		});
-	conn.query('CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, room TEXT, nickname TEXT, body TEXT, time INTEGER);')
+	conn.query(createMessagesTable)
 		.on('error', function() {
 			console.log("ERROR: messages table could not be created!!".error);
 		});
@@ -56,11 +81,11 @@ app.get('/', function(req, resp) {
 app.get('/recentRooms.json', function(req, resp) {
 	console.log('- Request received:', req.method.cyan, req.url.underline);
 	var data = {recentRooms: []};
-	//get all rooms active within last 5 min:
-	var q = conn.query("SELECT DISTINCT room FROM messages WHERE time >= strftime('%s', 'now') - 300;");
+	//get all recently active rooms:
+	var q = conn.query(getRecentRooms);
 	q.on('row', function(row) {
 		console.log("The returned row object:", row);
-		data.recentRooms.push(row.room); //TODO: only need room's string name
+		data.recentRooms.push(row.room);
 	});
 	q.on('end', function() {
 		console.log('Finished searching for recent rooms'.cyan);
@@ -72,13 +97,17 @@ app.get('/recentRooms.json', function(req, resp) {
 
 app.post('/:roomName/create', function(req, resp) {
 	var data = {roomExists: false};	//response data to send (json) roomExists starts as false
-	var q = conn.query('SELECT name FROM rooms WHERE name=$1;', [req.params.roomName]);
+
+	//check if the room exists
+	var q = conn.query(getRoomByName, [req.params.roomName]);
 	q.on('row', function(row) {
 		data.roomExists = true; //if we get here we know that the room is in the database.
 	});
+
+	//if room does not exist, put it in database.
 	q.on('end', function() {
-		if (!data.roomExists) //if room does not exist, put it in database.
-			conn.query("INSERT INTO rooms VALUES ($1);", [req.params.roomName]);
+		if (!data.roomExists)
+			conn.query(insertRoom, [req.params.roomName]);
 		resp.json(data);
 	});
 });
@@ -86,18 +115,19 @@ app.post('/:roomName/create', function(req, resp) {
 
 
 app.get('/:roomName', function(req, resp) {
+	var roomName = req.params.roomName;
 	console.log('- Request received:', req.method.cyan, req.url.underline);
 	//first we check if room exists:
 	var isRoom = false;
-	var q = conn.query('SELECT name FROM rooms WHERE name=$1;', [req.params.roomName]);
+	var q = conn.query(getRoomByName, [roomName]);
 	q.on('row', function(row) { 
-		console.log("Request for room " + req.params.roomName + " validated");
+		console.log("Request for room " + roomName + " validated");
 		isRoom = true;
 	});
 	q.on('error', function() {
 		//probably the rooms table does not exist. 
 		//provide link to '/' so that table can be created
-		console.log("ERROR: database could not be queried.");
+		console.log("ERROR looking for room" + roomName);
 		resp.writeHead(200, {'Content-Type': 'text/html'});
 		resp.end("<h1>Uh Oh! An error occurred.</h1> <h3><a href='/'>Click here</a> to try a restart.<h3>");
 		return;
@@ -105,66 +135,41 @@ app.get('/:roomName', function(req, resp) {
 	q.on('end', function() {
 		if (!isRoom) { //if room does not exist, give an error.
 			resp.status(404).end("Error 404: room not found"); //throw 404
-			var msg = "Room '"+ req.params.roomName + "' does not exist in database!";
+			var msg = "Room '" + roomName + "' does not exist in database!";
 			console.log(msg.red);
 		}
 		else {
 			//render chatroom for name
-			console.log('Entered Room:', req.params.roomName.yellow);
-			resp.render('room.html', {roomName: req.params.roomName});
+			console.log('Loading Room:', roomName.yellow);
+			resp.render('room.html', {roomName: roomName});
 		}
 	});
 });
 
 
-app.post('/:roomName/messages/prev', function(req, resp) {
-	//console.log("Get request received for only recent messages:", req.body);
-	var data = {msgArray:[], success:true};//response data
-
-	//get all messages from the room that occur after the ID specified by the client.
-	var q = conn.query("SELECT * FROM messages WHERE id > $1 AND room=$2;", [req.body.prevID, req.params.roomName] );
-	q.on('row', function(row) {
-		console.log("New message in "+ req.params.roomName +":", row.nickname+": " + row.body);
-		data.msgArray.push(row);
-	});
-	q.on('error', function() {
-		console.log("There was a problem getting the desired messages from the database");
-		data.success = false;
-	});
-	q.on('end', function() {
-		resp.json(data);
-	});
-});
-
-app.get('/:roomName/messages', function(req, resp) {
-	console.log("Get request received for all messages:", req.body);
-	var data = {msgArray:[], success:true};//response data
-
-	var q = conn.query("SELECT * FROM messages WHERE room=$1;", [req.params.roomName] ); //get all messages for the room
-	q.on('row', function(row) {
-		//console.log("Found message:", row);
-		data.msgArray.push(row);
-	});
-	q.on('error', function() {
-		console.log("There was a problem getting the desired messages from the database");
-		data.success = false;
-	});
-	q.on('end', function() {
-		resp.json(data);
-	});
-});
-
-
+/**
+ * Inserts a new message into the database
+ * @param  {JSON} req  {
+ *                      message: 'to send',
+ *                      nickname: 'of_sender'
+ *                     }
+ * 
+ * @param  {JSON} resp {
+ *                      succcess: true or false
+ *                     }
+ */
 app.post('/:roomName/messages', function(req, resp) {
 	console.log("Message post received:", req.body);
 	var data = {success: true};
-	//table schema:
-	//id INTEGER PRIMARY KEY AUTOINCREMENT, room TEXT, nickname TEXT, body TEXT, time INTEGER
-	var q = conn.query("INSERT INTO messages(room, nickname, body, time) VALUES($1, $2, $3, $4);", [req.params.roomName, req.body.nickname, req.body.message, Math.round(Date.now()/1000)] );
+
+	//insert new message
+	//.schema: id INTEGER, room TEXT, nickname TEXT, body TEXT, time INTEGER
+	var q = conn.query(insertMessage, [req.params.roomName, req.body.nickname, req.body.message, Math.round(Date.now()/1000)] );
 	q.on('error', function() {
 		console.log("Problem inserting message into messages table!");
 		data.success = false;
 	});
+	
 	q.on('end', function() {
 		resp.json(data);
 	});
@@ -172,5 +177,198 @@ app.post('/:roomName/messages', function(req, resp) {
 
 
 
+/* 
+   CLIENT HANDLER (socket.io)
+   ========================================================================== */
+io.sockets.on('connection', function(socket) {
+	
+	/* New users */
+	socket.on('join', function(roomName, nickname, callback) {
+		console.log("User " + nickname.magenta + " joined room " + roomName.yellow);
+		socket.join(roomName); //socket.io rooms feature
+		socket.nickname = nickname; // set nickname field of this socket
+		socket.active = true;
+
+		broadcastMembership(roomName); //update userlist
+
+		//get all messages for the room
+		var messages = [];
+		var q = conn.query(getMessagesInRoom, [roomName] );
+
+		q.on('row', function(row) {
+			messages.push(row); //put each message found in the list
+		});
+
+		q.on('error', function() {
+			console.log("ERROR retrieving messages.\n Room: ".error + roomName);
+			data.success = false;
+		});
+
+		//send the messages to the client
+		q.on('end', function() {
+			callback(messages);
+		});
+	});
+
+
+	/* If a client changes their nickname */
+	socket.on('nickname', function(nickname) {
+		console.log("User " + socket.nickname + " changing name to " + nickname.magenta);
+		socket.nickname = nickname;
+	});
+
+
+	/* Handle incoming messages: */
+	socket.on('message', function(msgBody) {
+		//get room socket is in
+		var roomName = getCurrRoom(socket);
+
+		console.log("New message in room " + roomName.yellow + " from user " + socket.nickname.magenta);
+		var msg = toMsgString(msgBody); //make the message HTML-safe
+		var q = conn.query(insertMessage, [roomName, socket.nickname, msg, Math.round(Date.now()/1000)] );
+		
+		q.on('error', function() {
+			console.log("ERROR inserting new message into database!");
+			return; //don't want to send the message bc it's not in the database
+		});
+
+		//Send the new message to everyone in the room
+		q.on('end', function() {
+			broadcastMessage(roomName, socket.nickname, msg);
+		});
+	});
+
+	/* Handle client disconnect */
+	socket.on('disconnect', function(){
+		broadcastMembership(getCurrRoom(socket));
+    });
+
+	socket.on('blur', function() {
+		socket.active = false;
+		broadcastMembership(getCurrRoom(socket));
+	});
+
+	socket.on('focus', function() {
+		socket.active = true;
+		broadcastMembership(getCurrRoom(socket));
+	});
+
+});
+
+
+/* 
+   Helper methods for socket
+   ========================================================================== */
+
+/**
+ * Gets the current room that the parametized client socket
+ * is in. Returns empty string if the client is only in the 
+ * global '' room.
+ * 
+ * @param  {Client Socket} socket
+ * @return {String} The room name that the socket is in, or '' if 
+ *                  the socket is only in the global room        
+ */
+function getCurrRoom(socket) {
+	var currentRooms = Object.keys(io.sockets.manager.roomClients[socket.id]);
+	if (!currentRooms) return '';
+	//all clients are in a global room ''
+	//must sub string bc rooms are prefixed by '/'
+	return (currentRooms[0] === '' && currentRooms.length > 1) ? currentRooms[1].substr(1) : currentRooms[0].substr(1);
+}
+
+
+/**
+ * Sends a new message to all sockets in a given room
+ * 
+ * @param  {String} roomName - the name of the room who's getting a new message
+ */
+function broadcastMessage(roomName, nickname, message) {
+	io.sockets.in(roomName).emit('message', nickname, message);
+}
+
+
+/**
+ * Tells all the sockets in the particular room
+ * that the membership has changed
+ * (e.g. someone left, joined, or changed
+ *  their name)
+ * 
+ * @param	{String} roomName - the name of the room whose
+ *                           membership is changing
+ */
+function broadcastMembership(roomName) {
+	if (!roomName) return; //gtfo if empty string or null or undef
+
+	console.log("Membership of " + roomName.yellow + " has changed.");
+	var sockets = io.sockets.clients(roomName); //get all the pertinent clients
+	
+	// get all the nicknames from the clients using arr.map()
+	var userlist = sockets.map(function(socket) {
+		return {
+			nickname: socket.nickname,
+			active:	socket.active	
+		};
+	});
+
+	//broadcast:
+	io.sockets.in(roomName).emit('membershipChanged', userlist);
+}
+
+
+
+
+
+/**
+ * Takes an 'unsafe' message and determines if it is a link to a URL or an image.
+ * If it is determined to be an image, it is rendered as such by adding <img>
+ * tags.
+ * If it is determined to be a URL, it is rendered as a clickable link by adding
+ * <a> tags.
+ * If it is not a link or an image, any and all HTML is escaped
+ * so that it will not interfere with the page HTML.
+ * 
+ * @param  {String} unsafe - The message body (which may contain a link or other HTML)
+ * @return {String} A string which conatins the original message in safe or escaped HTML.
+ */
+function toMsgString(unsafe) {
+	if (!unsafe) return '';
+	//precompile some regular expressions to save time:
+	var url_regex = /^(http|https)\:\/\/[\w\d\-_]+(\.[\w\d\-_]+)+([\w\-\.,@?^=%&amp;\:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/; //url matching regex i found 
+	var img_regex = /(.\.png|.\.gif|\.jpeg|.\.jpg)/; //image-URL matching regular expression
+	var url_arr = unsafe.match(url_regex); //test if its a url
+	if (url_arr) {
+		if (unsafe.substr(unsafe.length - 5).match(img_regex)) {
+			//it's an image!
+			return "<img style='max-width:500px;' src='" + unsafe + "'>";
+		} else {
+			//its just a url
+			return "<a href='" + unsafe + "'>" + unsafe + "</a>";
+		}
+	} else {
+		//it's not a URL. convert it to an HTML-escaped string
+		return escapeHtml(unsafe);
+	}
+}
+
+/**
+ * Escapes all special HTML characters that may
+ * cause problems when being rendered client-side.
+ * @param  {String} unsafe - the message to be escaped
+ * @return {String}          the same method with any HTML escaped.
+ */
+function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
+
+/*
 //listening to port 8080
+//no longer using this! listen with server instead
 app.listen(8080);
+*/
+server.listen(8080);
